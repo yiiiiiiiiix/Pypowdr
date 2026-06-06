f0317f97715e76c6e74f66ab65c55fe770a80451
Yi Xiang <yxiangswust@foxmail.com>
2026-06-06 17:30:57 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Pypowdr: Python tools for automated full pattern summation (FPS) analysis
+of X-ray powder diffraction data.
+
+This package implements the automated FPS workflow (afps) described by
+Butler & Hillier (2021), Computers & Geosciences, 147, 104662.
+"""
+
+__version__ = "0.1.0"
